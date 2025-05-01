2991cac9b5f54847b5f902dea2d1b0146a8a3634
WoodhansJ <woodhansjoseph11@gmail.com>
2025-05-01 12:19:41 -0400

    Admin can view event statistics.

--- src/main/java/frontend/src/pages/Events.js
+++ src/main/java/frontend/src/pages/Events.js
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import {
     collection, getDocs, addDoc, serverTimestamp, deleteDoc,
     doc, getDoc, updateDoc, query, where
